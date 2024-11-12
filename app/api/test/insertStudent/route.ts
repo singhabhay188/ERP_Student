2899a33bf8c3b718316cc95961571b8b1174eb0a
Abhay Singh <singhchirag188@gmail.com>
2024-11-12 20:42:44 +0530

    created a test route to insert students in bulk

---
+++ app/api/test/insertStudent/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/db"
+import { Student } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+    const body = await req.json();
+    const { students } = body;
+
+    let nstudents = []
+
+    try{
+        for(let student of students){
+            let { enrollment, name, groupId, collegeId, imageUrl } = student;
+            let password = enrollment;
+    
+            const nstudent = await prisma.student.create({
+                data:{
+                    name,
+                    enrollment,
+                    password,
+                    imageUrl,
+                    groupId,
+                    collegeId
+                }
+            });
+            nstudents.push(nstudent);
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+
+    
+    
+    return NextResponse.json({students:nstudents});
+}
